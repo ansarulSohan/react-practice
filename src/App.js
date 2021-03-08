9509f6ee207185a082c444a9b03b677532f7f583
ansarulSohan <ansarulsohan@gmail.com>
2021-03-08 20:06:24 +0600

    Tutorial complete: Added a stateless functional component(NavBar)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import Counters from './components/counters';
 import NavBar from './components/navbar';
 
 class App extends Component {
-    state = {  }
+	state = { 
+		counters: [
+				{id: 1, value: 5},
+				{id: 2, value: 0},
+				{id: 3, value: 0},
+				{id: 4, value: 0}
+		]
+};
+
+handleIncrement = counter => {
+		const counters = [ ...this.state.counters];
+		const index = counters.indexOf(counter);
+		counters[index] = {...counter};
+		counters[index].value++;
+		this.setState({counters});
+}
+
+handleDelete = (counterID) => {
+		const counters = this.state.counters.filter(c =>  c.id !== counterID);
+		this.setState({counters})
+}
+
+handleReset = () => {
+		const counters = this.state.counters.map(c => {
+				c.value = 0;
+				return c
+		});
+		this.setState({counters});
+}
     render() { 
         return (
-            <div>
-        <NavBar />
-        <main className="container">
-            
-        </main>
-            </div>
+        <React.Fragment>
+            <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
+            <main className="container">
+							<Counters
+							onReset = {this.handleReset}
+							onIncrement = {this.handleIncrement}
+							onDelete = {this.handleDelete}
+							counters={this.state.counters}></Counters>
+						</main>
+        </React.Fragment>
+
         );
     }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NavBar extends Component {
-    state = {}
-    render() {
+
+//Stateless functional component
+
+
+const NavBar = ({totalCounters}) => {
         return (
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="/">Navbar
+                    <span className="badge badge-pill badge-secondary">{totalCounters}</span></a>
                 </div>
             </nav>
         );
-    }
-}
+};
+
 
 export default NavBar;
